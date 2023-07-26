9732b67b200083297b6d25b0d765e1230bf4f710
Mr. Worldwide <112043027+Momasosdigo@users.noreply.github.com>
2023-07-26 01:07:44 -0300

    eng.php

--- P&D web/BackOffice/lang/eng.php
+++ P&D web/BackOffice/lang/eng.php
@@ -1,70 +1,71 @@
 <?php
-//Ingles lang
-
-//Menu
-$lang['banderaIdioma'] = "img/flags/eng.png";
-$lang['lang-eng'] = 'English';
-$lang['lang-esp'] = 'Español';
-
-//barra lateral
-$lang['bLInicio'] = 'Home';
-$lang['bLRutas'] = 'Routes';
-$lang['bLCoches'] = 'Cars';
-$lang['bLUsuarios'] = 'Users';
-$lang['bLEstadísticas'] = 'Statistics';
-$lang['bLCerrarSesion'] = 'Logout';
-
-//inicio de sesion
-$lang['iniUsuario'] = ' User';
-$lang['iniClave'] = ' Password';
-$lang['iniBoton'] = 'LOGIN';
-$lang['iniRestablecer'] = 'Reset password';
-$lang['iniError'] = 'Enter the data well';
-
-//pagina usuarios
-$lang['userTitulo'] = 'Users Administration';
-$lang['userNuevo'] = ' New';
-$lang['userEditar'] = ' Edit';
-$lang['userBorrar'] = ' Delete';
-$lang['userBuscar'] = ' Search';
-$lang['userCedula'] = 'Document';
-$lang['userNombre'] = 'First name';
-$lang['userApellido'] = 'Last name';
-$lang['userPermisos'] = 'Permits';
-$lang['userNUsuario'] = 'New User';
-$lang['userContraseña'] = 'Password';
-$lang['userAtras'] = 'Back';
-$lang['userGuardar'] = 'Save';
-
-//pagina rutas
-$lang['rutasTitulo'] = 'Route Administration';
-$lang['rutasNuevo'] = ' New';
-$lang['rutasEditar'] = ' Edit';
-$lang['rutasBorrar'] = ' Delete';
-$lang['rutasBuscar'] = ' Search';
-$lang['rutasOrigen'] = 'Origin';
-$lang['rutasDestino'] = 'Destination';
-$lang['rutasNRuta'] = 'New Route';
-$lang['rutasIngresar'] = 'Enter';
-$lang['rutasHorarios'] = 'Timetables';
-$lang['rutasParadas'] = 'Stops';
-$lang['rutasAtras'] = 'Back';
-$lang['rutasGuardar'] = 'Save';
-$lang['rutasIHorario'] = 'Enter Timetable';
-$lang['rutasIParadas'] = 'Enter Stops';
-
-//pagina coches
-$lang['cochesTitulo'] = 'Car Administration';
-$lang['cochesNuevo'] = ' New';
-$lang['cochesEditar'] = ' Edit';
-$lang['cochesBorrar'] = ' Delete';
-$lang['cochesBuscar'] = ' Search';
-$lang['cochesCoche'] = 'Car';
-$lang['cochesMatricula'] = 'registration';
-$lang['cochesModelo'] = 'Model';
-$lang['cochesMarca'] = 'Brand';
-$lang['cochesAsientos'] = 'Seats';
-$lang['cochesPisos'] = 'Flats';
-$lang['cochesICoche'] = 'Enter Car';
-$lang['cochesAtras'] = 'Back';
-$lang['cochesGuardar'] = 'Save';
+  //Ingles lang
+  
+  //Menu
+  $lang['banderaIdioma'] = "img/flags/eng.png";
+  $lang['lang-eng'] = 'English';
+  $lang['lang-esp'] = 'Español';
+  
+  //barra lateral
+  $lang['bLInicio'] = 'Home';
+  $lang['bLRutas'] = 'Routes';
+  $lang['bLCoches'] = 'Cars';
+  $lang['bLUsuarios'] = 'Users';
+  $lang['bLEstadísticas'] = 'Statistics';
+  $lang['bLCerrarSesion'] = 'Logout';
+  
+  //inicio de sesion
+  $lang['iniUsuario'] = ' User';
+  $lang['iniClave'] = ' Password';
+  $lang['iniBoton'] = 'LOGIN';
+  $lang['iniRestablecer'] = 'Reset password';
+  $lang['iniError'] = 'Enter the data well';
+  
+  //pagina usuarios
+  $lang['userTitulo'] = 'Users Administration';
+  $lang['userNuevo'] = ' New';
+  $lang['userEditar'] = ' Edit';
+  $lang['userBorrar'] = ' Delete';
+  $lang['userBuscar'] = ' Search';
+  $lang['userCedula'] = 'Document';
+  $lang['userNombre'] = 'First name';
+  $lang['userApellido'] = 'Last name';
+  $lang['userPermisos'] = 'Permits';
+  $lang['userNUsuario'] = 'New User';
+  $lang['userContraseña'] = 'Password';
+  $lang['userAtras'] = 'Back';
+  $lang['userGuardar'] = 'Save';
+  
+  //pagina rutas
+  $lang['rutasTitulo'] = 'Route Administration';
+  $lang['rutasNuevo'] = ' New';
+  $lang['rutasEditar'] = ' Edit';
+  $lang['rutasBorrar'] = ' Delete';
+  $lang['rutasBuscar'] = ' Search';
+  $lang['rutasOrigen'] = 'Origin';
+  $lang['rutasDestino'] = 'Destination';
+  $lang['rutasNRuta'] = 'New Route';
+  $lang['rutasIngresar'] = 'Enter';
+  $lang['rutasHorarios'] = 'Timetables';
+  $lang['rutasParadas'] = 'Stops';
+  $lang['rutasAtras'] = 'Back';
+  $lang['rutasGuardar'] = 'Save';
+  $lang['rutasIHorario'] = 'Enter Timetable';
+  $lang['rutasIParadas'] = 'Enter Stops';
+  
+  //pagina coches
+  $lang['cochesTitulo'] = 'Car Administration';
+  $lang['cochesNuevo'] = ' New';
+  $lang['cochesEditar'] = ' Edit';
+  $lang['cochesBorrar'] = ' Delete';
+  $lang['cochesBuscar'] = ' Search';
+  $lang['cochesCoche'] = 'Car';
+  $lang['cochesMatricula'] = 'registration';
+  $lang['cochesModelo'] = 'Model';
+  $lang['cochesMarca'] = 'Brand';
+  $lang['cochesAsientos'] = 'Seats';
+  $lang['cochesPisos'] = 'Flats';
+  $lang['cochesICoche'] = 'Enter Car';
+  $lang['cochesAtras'] = 'Back';
+  $lang['cochesGuardar'] = 'Save';
+?>
