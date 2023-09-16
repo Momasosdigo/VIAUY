2475afca30a107fa8a97deb364d4fb81f77d7e76
agustinduarte <34344135+agustinduarte@users.noreply.github.com>
2023-09-16 00:54:20 -0300

    Add files via upload

---
+++ P&D web/viauyUsuario/js/main.js
@@ -0,0 +1,15 @@
+function siguientePag(){
+    window.location="paso-1.html";
+}
+
+function siguientePag1(){
+    window.location="paso-2.html";
+}
+
+function siguientePag2(){
+    window.location="paso-3.html";
+}
+
+function siguientePag3(){
+    window.location="completado.html";
+}
